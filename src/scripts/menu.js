e3e3fee7018ee8b45e03f18bf6bf34099f8217e8
Hunter <hvanlear@gmail.com>
2023-09-26 21:00:34 -0400

    placeholder contact section

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,7 +1,10 @@
 const primaryNav = document.querySelector('.primary-nav')
 const hamButton = document.querySelector('.nav-toggle')
+const contactNavLink = document.querySelector('#nav-link-contact')
 
-hamButton.addEventListener('click', () => {
+//function to open and close the nav container
+
+let menuToggle = () => {
 	const vis = primaryNav.getAttribute('data-visible')
 	if (vis === 'false') {
 		primaryNav.setAttribute('data-visible', true)
@@ -10,4 +13,15 @@ hamButton.addEventListener('click', () => {
 		primaryNav.setAttribute('data-visible', false)
 		hamButton.setAttribute('aria-expanded', false)
 	}
+}
+
+//Close the menu overlay when clicking on the contact button
+//menu was not closing becuase it's only moving the viewport to the bototm of the screen
+contactNavLink.addEventListener('click', () => {
+	menuToggle()
+})
+
+//close menu overlay
+hamButton.addEventListener('click', () => {
+	menuToggle()
 })
